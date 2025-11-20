d2f49bd470673842885b8e17126f07855be9f804
Blueweb2 <info@blueweb2.com>
2025-11-20 10:39:24 +0530

    changed route of media

--- routes/mediaRoutes.js
+++ routes/mediaRoutes.js
@@ -53,7 +53,7 @@ function safeResolve(folder, fileName) {
 /* -----------------------------------------
     GET /api/media  (pagination + search + filter)
 ------------------------------------------*/
-router.get("/api/media", async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const page = Number(req.query.page) || 1;
     const limit = Number(req.query.limit) || 40;
